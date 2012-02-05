3f66860f3e491093e6f0d252b67d5efd63f6ae2e
Peter Brindisi <peter.brindisi@gmail.com>
2012-02-05 20:49:19 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paper::Application.config.secret_token = '99b76a6b9e332e4cd309ef223a7262f381304f7f47e3e86f8b4a00c6272620757ee932ea21aa53701a62770fa0c025c1db8752763df9f6215afa98cc543b1ce6'
