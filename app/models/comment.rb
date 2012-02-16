3920843073f33e0c42a3cdb1816643faf4c61ad6
Peter Brindisi <peter.brindisi@gmail.com>
2012-02-16 12:52:09 +0100

    adds (untested) comment model and (unfinished) tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,20 @@
+# a comment's default privacy is public
+# a reply to a private comment is always private
+# a private comment cannot become public (maybe change in the future)
 class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
   
   include Paper::Markdown
+  include Paper::Privacy
   
-  before_validation :set_post, :on => :create
-  before_validation :set_path, :on => :create
+  before_validation :set_post,       :on => :create
+  before_validation :set_path,       :on => :create
+  before_validation :set_visible_to, :on => :create
+  
+  before_save  :inherit_privacy
+  after_save   :pass_down_privacy
+  after_create :add_user_to_parent_visible
   
   belongs_to :user
   belongs_to :post
@@ -13,9 +22,21 @@ class Comment
   
   has_many :comments, :foreign_key => :parent_id
   
-  validates_presence_of :user_id, :post_id, :path
+  validates_presence_of :user_id, :post_id, :path, :visible_to
+  validates_presence_of :body
   
-  field :path, :type => Array
+  field :path,       :type => Array
+  field :visible_to, :type => Array, :default => [ ]
+  
+  alias_method :body, :markdown
+  
+  def self.visible_to(user)
+    if user
+      any_of({ :privacy => Paper::PRIVACY[:public] }, { :visible_to => { '$in' => [ user.id ] } })
+    else
+      where(:privacy => Paper::PRIVACY[:public])
+    end
+  end
   
   protected
     
@@ -36,6 +57,51 @@ class Comment
         self.path = self.parent.path + [ self.id ]
       else
         self.path = [ self.id ]
+      end
+    end
+    
+    # before_validation, :on => :create
+    # set the default list of users who can 
+    # view this comment
+    # a comment is always visible to the owner
+    # a comment is always visible to the post owner
+    # a comment is always visible to the parent comment owner
+    def set_visible_to
+      
+      if self.user
+        self.visible_to << self.user.id
+      end
+      
+      if self.post
+        self.visible_to |= [ self.post.user.id ]
+        if self.parent
+          self.visible_to |= [ self.parent.user.id ]
+        end
+      end
+    end
+    
+     # set privacy to private if parent or post is private
+    def inherit_privacy
+      if self.parent.try(:private?) || self.post.try?(:private?)
+        self.private! unless self.private?
+      end
+    end
+    
+    # after_save
+    # if our privacy has changed to private, set children to private
+    def pass_down_privacy
+      if self.changes.has_key?('privacy') && self.private?
+        self.comments.each { |child| child.private! unless child.private? }
+      end
+    end
+    
+    # after_create
+    # a comment is always visible to the owners of child comments
+    def add_user_to_parent_visible(p = self.parent)
+      if p
+        p.visible_to |= [ self.user.id ]
+        p.save
+        add_user_to_parent_visible(p.parent)
       end
     end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'visible to' do
+    
+    before :each do
+      @user = Fabricate(:user)
+      @post = Fabricate(:post)
+    end
+    
+    describe 'to owner'
+    describe 'to parent owner'
+    describe 'to children owners'
+    describe 'to childrens children owners'
+  end
 end
